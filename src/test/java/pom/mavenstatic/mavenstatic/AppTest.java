a328757c3d6e012137409067749b3878a31dae26
Nishita <challanishita07@gmail.com>
2022-11-29 00:16:57 +0530

    commit 2

---
+++ src/test/java/pom/mavenstatic/mavenstatic/AppTest.java
@@ -0,0 +1,20 @@
+package pom.mavenstatic.mavenstatic;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
